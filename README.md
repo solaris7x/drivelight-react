0c0c65e8f8f37c44ccdd5abfde543f9e8c3eb72f
solaris7x <question211199@gmail.com>
2022-05-18 17:28:50 +0530

    Vite + tailwind

---
+++ README.md
@@ -0,0 +1,7 @@
+# Driveligh-React
+
+Makeshift react app to list contents of google drive folder
+
+Provide direct link to all files 🙀🙀
+
+Note: This is independent of other drivelight projects
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-pink-700 text-white flex justify-center items-center min-h-screen">
+      <div className="text-4xl font-bold text-center">DriveLight</div>
+    </div>
+  );
+};
+
+export default App;
